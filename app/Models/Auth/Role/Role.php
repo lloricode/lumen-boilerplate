61f37aa18586e2df52ad5925f3c00a83181e59fc
Lloric Mayuga Garcia <lloricode@gmail.com>
2019-11-05 06:19:59 +0800

    Ide helper
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- app/Models/Auth/Role/Role.php
+++ app/Models/Auth/Role/Role.php
@@ -14,11 +14,6 @@ use Illuminate\Database\Eloquent\Builder;
 /**
  * App\Models\Auth\Role\Role
  *
- * @property int $id
- * @property string $name
- * @property string $guard_name
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auth\Permission\Permission[] $permissions
  * @property-read int|null $permissions_count
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auth\User\User[] $users
@@ -27,11 +22,6 @@ use Illuminate\Database\Eloquent\Builder;
  * @method static Builder|Role newQuery()
  * @method static Builder|\Spatie\Permission\Models\Role permission($permissions)
  * @method static Builder|Role query()
- * @method static Builder|Role whereCreatedAt($value)
- * @method static Builder|Role whereGuardName($value)
- * @method static Builder|Role whereId($value)
- * @method static Builder|Role whereName($value)
- * @method static Builder|Role whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Role extends \Spatie\Permission\Models\Role
