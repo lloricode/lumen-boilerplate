b10d281f8a3cfd62bb0679f2a75fc86fdd565cc2
Lloric Mayuga Garcia <lloricode@gmail.com>
2020-01-09 07:50:43 +0800

    :recycle: Format PSR-12
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -2,6 +2,6 @@
 
 return [
 
-    'welcome' => 'Bienvenido a ' . env('APP_NAME', 'Lumen 5.7 Dingo Boilerplate'),
+    'welcome' => 'Bienvenido a '.env('APP_NAME', 'Lumen 5.7 Dingo Boilerplate'),
 
 ];
