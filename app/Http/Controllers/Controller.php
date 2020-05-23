bab684203f004586ef1fa5833c25ca4ad2049dd1
Lloric Mayuga Garcia <lloricode@gmail.com>
2020-05-23 20:43:04 +0800

    Use default config
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Traits\Hashable;
 use App\Transformers\BaseTransformer;
 use Laravel\Lumen\Routing\Controller as BaseController;
+use League\Fractal\Serializer\JsonApiSerializer;
 
 class Controller extends BaseController
 {
@@ -18,7 +19,7 @@ class Controller extends BaseController
      */
     protected function fractal($data, BaseTransformer $transformer)
     {
-        return fractal($data, $transformer)
+        return fractal($data, $transformer, JsonApiSerializer::class)
             ->withResourceName($transformer->getResourceKey())
             ->addMeta('include', $transformer->getAvailableIncludes());
     }
