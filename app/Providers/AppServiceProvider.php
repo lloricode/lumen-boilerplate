75aa7c2d1992be403635c25b1b6764c9dc59a297
Lloric Mayuga Garcia <lloricode@gmail.com>
2020-05-23 21:08:39 +0800

    hashClientSecrets
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
             Schema::defaultStringLength(191);
         }
 
+        Passport::hashClientSecrets();
+
         LumenPassport::routes(
             $this->app->router,
             [
