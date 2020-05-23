52e63101d4862e5132fd1a0c31c881c237f0a94b
Lloric Mayuga Garcia <lloricode@gmail.com>
2020-05-23 20:39:45 +0800

    :coffee: Normalise base controller
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Traits\Hashable;
 use App\Transformers\BaseTransformer;
 use Laravel\Lumen\Routing\Controller as BaseController;
-use Spatie\Fractal\Fractal;
 
 class Controller extends BaseController
 {
@@ -13,43 +12,14 @@ class Controller extends BaseController
 
     /**
      * @param $data
-     * @param $transformer
+     * @param  \App\Transformers\BaseTransformer  $transformer
      *
      * @return \Spatie\Fractal\Fractal
      */
-    protected function fractal($data, $transformer)
+    protected function fractal($data, BaseTransformer $transformer)
     {
-        $fractal = fractal($data, $transformer)
-            ->withResourceName($this->getResourceKey($transformer));
-
-        return $this->addAvailableIncludes($fractal, $transformer);
-    }
-
-    /**
-     * @param $transformer
-     *
-     * @return string
-     */
-    private function getResourceKey($transformer): string
-    {
-        return $this->checkTransformer($transformer)->getResourceKey();
-    }
-
-    /**
-     * @param $transformer
-     *
-     * @return \App\Transformers\BaseTransformer
-     */
-    private function checkTransformer($transformer): BaseTransformer
-    {
-        if (is_string($transformer)) {
-            $transformer = app($transformer);
-        }
-        return $transformer;
-    }
-
-    private function addAvailableIncludes(Fractal $response, $transformer): Fractal
-    {
-        return $response->addMeta('include', $this->checkTransformer($transformer)->getAvailableIncludes());
+        return fractal($data, $transformer)
+            ->withResourceName($transformer->getResourceKey())
+            ->addMeta('include', $transformer->getAvailableIncludes());
     }
 }
