e1ca865ea19b77b3a1610fab6fee126bd409ae2b
Syauqi <syauqisnh@gmail.com>
2022-12-06 16:57:04 +0700

    Menambahkan File Transformer Pemilik_Kos

---
+++ app/Transformers/PemilikKos_Transformers.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+
+class PemilikKosTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected array $defaultIncludes = [
+        //
+    ];
+    
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected array $availableIncludes = [
+        //
+    ];
+    
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform()
+    {
+        return [
+            //
+        ];
+    }
+}
