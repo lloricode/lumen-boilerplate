f58485df783169ca08ea5104ca1132f8cce4e38d
Syauqi <syauqisnh@gmail.com>
2022-12-06 16:42:53 +0700

    Membuat File PemilikKos_Conroller

---
+++ app/Http/Controllers/PemilikKos_Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Transformers\BaseTransformer;
+use Laravel\Lumen\Routing\Controller as BaseController;
+use League\Fractal\Serializer\JsonApiSerializer;
+
+class PemilikKos_Controller extends BaseController
+{
+}
